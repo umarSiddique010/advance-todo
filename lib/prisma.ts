1821057a9f9c66b6c7f432dadd77ef201f53bb21
Md Umar Siddique <us70763@gamil.com>
2026-02-20 16:04:27 +0530

    switch back to prisma-client-js

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,10 @@
-import { PrismaClient } from '../app/generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@/app/generated/prisma';
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+  prisma: PrismaClient | undefined;
 };
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-  });
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
